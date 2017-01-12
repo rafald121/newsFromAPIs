80875d782fe2f618a7f817d0959f43055f5fe187
rafald121 <rafald121@gmail.com>
2017-01-12 12:51:30 +0100

    found how to retrieve list from AsynTaskLoader. ready to go ahead

--- app/src/main/java/com/example/android/newsabouttechnology/MainActivity.java
+++ app/src/main/java/com/example/android/newsabouttechnology/MainActivity.java
@@ -9,10 +9,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 
+import static android.R.attr.data;
 import static android.R.id.list;
+
 
 public class MainActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<List<News>> {
@@ -21,7 +24,9 @@ public class MainActivity extends AppCompatActivity
             ".org/v1/articles?source=engadget&sortBy=top&apiKey=a25a3dfb39d94d448c334519daf57be2";
     private static final int EARTHQUAKE_LOADER_ID = 1;
 
-    List<News> hao;
+    Button button;
+
+    List<News> newsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +35,10 @@ public class MainActivity extends AppCompatActivity
 
         setContentView(R.layout.activity_main);
 
+
         
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
-
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
         if (networkInfo != null && networkInfo.isConnected()) {
@@ -41,15 +46,15 @@ public class MainActivity extends AppCompatActivity
             loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
 
         } else {
-
+            Log.e(TAG, "onCreate: ŁADUJE INFO" );
         }
-        Log.i(TAG, "onCreate: drukujem");
 
-        System.out.println(hao.toString());
 
         Log.i(TAG, "onCreate: END");
 
     }
+
+
 
 
     @Override
@@ -57,8 +62,7 @@ public class MainActivity extends AppCompatActivity
         Log.i(TAG, "onCreateLoader: START");
 
         NewsLoader newsloader = new NewsLoader(this,URL);
-        hao  = newsloader.getListOfNews();
-        Log.i(TAG, "onCreateLoader: list: " + list);
+
         Log.i(TAG, "onCreateLoader: END");
 
         return newsloader;
@@ -67,6 +71,14 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onLoadFinished(Loader<List<News>> loader, List<News> data) {
         Log.i(TAG, "onLoadFinished: START");
+
+        if(newsList!=null)
+            newsList = data;
+        else
+            Log.e(TAG, "onLoadFinished: HAO IS NULL" );
+
+        Log.i(TAG, "onLoadFinished: listaZNewsami: " + newsList.toString());
+
 
     }
 
